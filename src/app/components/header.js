c75612f7060cfb11d0e29c12e5cfd56ae5be4b6a
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-27 17:33:47 +0200

    portfolio

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -11,20 +11,20 @@ export default function Header({ language, setLanguage }) {
   };
 
   return (
-    <header className="relative w-full  flex justify-between lg:p-10 pt-10 pb-10 pl-5 pr-5 mb-64 lg:text-2xl text-sm font-bold hover:bg-blue-400 duration-300 hover:bg-opacity-60">
+    <header className="relative w-full  flex justify-between lg:p-10 pt-10 pb-10 pl-4 pr-4 mb-64 lg:text-2xl text-sm font-bold hover:bg-blue-400 duration-300 hover:bg-opacity-60">
       <div className=" px-2 text-left text-white ">Rubén Pérez Lara</div>
 
       <div className="flex ">
         <div
           onClick={toggleLanguage}
-          className="cursor-pointer text-right text-white lg:mr-10  hover:bg-blue-600 rounded-xl px-2  hover:scale-125 duration-300"
+          className="cursor-pointer text-right text-white lg:mr-10  hover:bg-blue-600 rounded-xl px-2 pb-1 lg:hover:scale-125 hover:scale-110 duration-300  mr-1"
         >
           <span className="">{language === "ES" ? "Inglés" : "Spanish"}</span>
         </div>
 
         <a
           href="mailto:inforubentrabajos@gmail.com"
-          className="cursor-pointer text-right text-white lg:mr-10  hover:bg-blue-600 rounded-xl px-2  hover:scale-125 duration-300"
+          className=" cursor-pointer text-right text-white lg:mr-10  hover:bg-blue-600 rounded-xl px-2 pb-1  lg:hover:scale-125 hover:scale-110 duration-300"
         >
           {languages[language].contact}
         </a>
