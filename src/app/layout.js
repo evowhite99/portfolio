83f9d6a1af4ee64711298aa7b6ff8b353fe9deaf
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-30 19:01:18 +0200

    portfolio

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
         <link rel="icon" href="images/favicon.ico" />
       </head>
       <body className={`${inter.className}  `}>
