2cf8f062b73773e1fa7ff6e1f38299a4c5f0446c
rubenwebportfolio <gtpruben13@gmail.com>
2024-08-30 15:30:32 +0200

    portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,16 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-to-bottom":
+          "linear-gradient(to bottom, transparent, rgb(var(--background-end-rgb))) rgb(var(--background-start-rgb))",
+      },
+      colors: {
+        foreground: "rgb(var(--foreground-rgb))",
+        backgroundStart: "rgb(var(--background-start-rgb))",
+        backgroundEnd: "rgb(var(--background-end-rgb))",
+      },
+      boxShadow: {
+        custom: "0 0 15px rgba(0, 0, 0, 0.3), 0 0 15px rgba(0, 0, 0, 0.3)",
       },
     },
   },
